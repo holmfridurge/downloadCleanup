a70593ce9da418dd96fdd17e146fa0e8302b8060
perlahj <perlahj@gmail.com>
2016-05-05 23:45:26 +0000

    newnames

--- regexPathTestFile.py
+++ regexPathTestFile.py
@@ -10,12 +10,11 @@ repseason = '((S|s|Season|season)(\d{1,2})(E|e|Episode|episode)(\d{1,2})|(\[\d{1
 regex = r'(.*)([ -_\.]){1}' + repseason + '(.)*' + rending
 
 
-
-def compress(s):
+def compressname(s):
         return re.sub('[(){}<>\-\.; ]', '', s).lower()
 
 # TO DO: gripa villu tegar thattur sem leitar er af er ekki til
-# Geta slad inn partial nofn
+# TO D0: gripa se og compressa
     
 def ff(direct,s):
 
@@ -25,16 +24,20 @@ def ff(direct,s):
     repseason = '((S|s|Season|season)(\d{1,2})(E|e|Episode|episode)(\d{1,2})|(\[\d{1,2}[.x]\d{1,2}\]))'
     regex = r'(.*)([ -_\.]){1}' + repseason + '(.)*' + rending
     
-    name = compress(s)
+    name = compressname(s)
 
     allshows = [os.path.join(root,file) for root,_,files in os.walk(direct) for file in files]
     sorshows = [show for show in allshows if re.search(regex,show.split('\\')[-1]) != None
-                     and name in compress(re.search(regex,show).group(1).split('\\')[-1])]
-    return sorshows
+                     and name in compressname(re.search(regex,show).group(1).split('\\')[-1])]
+
+    newnames = [compressname(re.search(regex,show).group(1).split('\\')[-1]) for show in sorshows]
+    
+    
+    return nsorshows
 
 #TESTS
-print(len(ff("downloads","8.Out.Of.10.Cats"))) #56
-print(len(ff("downloads","30 Rock"))) #36
-print(len(ff("downloads","30 Ro"))) #36
+#print(len(ff("downloads","8.Out.Of.10.Cats"))) #56
+#print(len(ff("downloads","30 Rock"))) #36
+#print(len(ff("downloads","30 Ro"))) #36
 
     
