7a4e50bfbf9a278b896f11d21bcf55131ed10fb7
holmfridurge <holmfridurge@gmail.com>
2016-05-06 15:05:55 +0000

    findmovies

--- findShows.py
+++ findShows.py
@@ -11,13 +11,14 @@ def findshows(direct,s):
     rending = '((\.avi|\.mkv|\.mp4|\.rar){1})'
     regex = '(.+)(([ -_\.]){0,1})' + rseason + '(.*)' + rending
     #regexlong = '(.+)(([ -_\.]){0,1})(((S|s|Season|season)(\d{1,2})(E|e|Episode|episode)(\d{1,2}))|((\[)(\d{1,2})([.xX-_])(\d{1,2})(\])))(.*)((\.avi|\.mkv|\.mp4|\.rar){1})'
-
     
     name = compressname(s)
 
     allshows = [os.path.join(root,file) for root,_,files in os.walk(direct) for file in files]
+    
     sshows = [show for show in allshows if re.search(regex,show.split('\\')[-1]) != None
                      and name in compressname(re.search(regex,show).group(1).split('\\')[-1])]
+    
     if not os.path.exists('Shows'):
         os.makedirs('Shows')
 
@@ -25,8 +26,11 @@ def findshows(direct,s):
         shutil.move(i, 'Shows/'+i.split('\\')[-1])
         print(i)
     # Returns a list containg the names of the shows that was searched for
-    #return sshows
+    return findmovies(direct, s)
 
+def findmovies(direct, s):
+    print('yay')
+    
 #TESTS
 #print(len(findshows("downloads","8.Out.Of.10.Cats")) #Result: 69
 #print(len(findshows("downloads","30 Rock"))) #Result: 61
