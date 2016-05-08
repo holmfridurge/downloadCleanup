888759eedad89a73950b531792fe11839c14d0c3
perlahj <perlahj@gmail.com>
2016-05-08 18:07:59 +0000

    all shows in downloads, op second para

--- findShowsRename.py
+++ findShowsRename.py
@@ -1,14 +1,15 @@
 import os
 import re
 
-#Use two different regex so we can use catched groups when compressins seasons
+#different groups for compressing
+#global so we wan use across files and functions
 rseason1 = '(S|s|Season|season)([ \-\_\.]*)(\d{1,2})([ \-\_\.]*)(E|e|Episode|episode)([ \-\_\.]*)(\d{1,2})'
 rseason2 = '(\[)*(\d{1,2})([.xX-_])(\d{1,2})(\])*'
 rseason3 = '(([-_\[#]+)(\d{1,3})(\]*))'
 rending = '((\.avi|\.mkv|\.mp4|\.rar|\.srt){1})'
 
 def compressname(s):
-    return re.sub('[\[\](){}<>\+\-\.\_;_ 0-9]', '', s).lower()
+    return re.sub('[\[\](){}<>\+\-\.\_;_ ]', '', s).lower()
 
 def compressseason(s):
     if re.search(rseason1,s):
@@ -18,25 +19,26 @@ def compressseason(s):
     else:
         return  'sx' + s[1:].lstrip('0')
 
-def findshows(direct,s):
+def findshows(direct,s = ''):
 
-    rseason = '(((S|s|Season|season)([ \-\_\.]*)(\d{1,2})([ \-\_\.]*)(E|e|Episode|episode)([ \-\_\.]*)(\d{1,2}))|((\[)(\d{1,2})([.xX-_])(\d{1,2})(\]))|(([-_#\[]+)(\d{1,3})(\])*))'
+    rseason = '(' + rseason1 + '|' + rseason2 + '|' + rseason3 + ')'
     regex = '(.+)(([ -_\.]){0,1})' + rseason + '(.*)' + rending
     
-    name = compressname(s)
-
-    allshows = [os.path.join(root,file) for root,_,files in os.walk(direct) for file in files]
-    sshows = [show for show in allshows if re.search(regex,show.split('\\')[-1]) != None
+    allfiles = [os.path.join(root,file) for root,_,files in os.walk(direct) for file in files]
+    if s == '':
+        sshows = [show for show in allfiles if re.search(regex,show.split('\\')[-1]) != None]
+    else:
+        name = compressname(s)
+        sshows = [show for show in allshows if re.search(regex,show.split('\\')[-1]) != None
                      and name in compressname(re.search(regex,show).group(1).split('\\')[-1])]
+    
 
     newnames = [compressname(re.search(regex,show).group(1).split('\\')[-1]) for show in sshows]
     newseasons = [compressseason(re.search(rseason,show).group(1).split('\\')[-1]) for show in sshows]
     endings = [re.search(rending,show).group(1).split('\\')[-1] for show in sshows]
     newshows = [name + "." + season + ending for name, season, ending in zip(newnames,newseasons,endings)]
-
-    returnShows = list(zip(sshows,newshows))
-    return returnShows
-    # Returns a list containg the names of the shows that was searched for and compressed filenames
-   # return [sshows,newshows]
+)
+    return zip(sshows,newshows)    
+   
 
 
