455fe5764de1831ee1840dd3c85be55d4e39a023
perlahj <perlahj@gmail.com>
2016-05-08 23:07:58 +0000

    finale!

--- findShowsRename.py
+++ findShowsRename.py
@@ -3,7 +3,7 @@ import re
 
 #global so we can use across files and functions
 rseason1 = '(S|s|Season|season)([ \-\_\.]*)(\d{1,2})([ \-\_\.]*)(E|e|Episode|episode)([ \-\_\.]*)(\d{1,2})'
-rseason2 = '(\[)*(\d{1,2})([\.xX\-\_ ])(\d{1,2})(\])*'
+rseason2 = '(\[)*(\d{1,2})([xX\-\_ ])(\d{1,2})(\])*'
 rseason3 = '(([\-\_\[#]+)(\d{1,3})(\]*))'
 rseason = '(' + rseason1 + '|' + rseason2 +  '|' + rseason3 + ')'
 rending = '((\.avi|\.mkv|\.mp4|\.rar|\.srt){1})'
@@ -36,18 +36,20 @@ def findshows(direct,s = ''):
     shortpaths = [show.split('\\')[-1] for show in fullpaths]
 
     #build new names for files
-    newnames = [compressname(re.search(regex,show).group(1).split('\\')[-1]) for show in shortpaths]
-    newseasons = [compressseason(re.search(rseason,show).group(1).split('\\')[-1]) for show in shortpaths]
-    endings = [re.search(rending,show).group(1).split('\\')[-1] for show in shortpaths]
+    newnames = [compressname(re.search(regex,show).group(1)) for show in shortpaths]
+    newseasons = [compressseason(re.search(rseason,show).group(1)) for show in shortpaths]
+    endings = [re.search(rending,show).group(1) for show in shortpaths]
     newshows = [name + "." + season + ending for name, season, ending in zip(newnames,newseasons,endings)]
 
     return zip(fullpaths,newshows)
 
 def findmovies(direct):
     allfiles = [os.path.join(root,file) for root,_,files in os.walk(direct) for file in files]
+    #movie: is not an episode, video format, a year (1900-2099) or no number in name.
     movies = [show for show in allfiles if re.search(regex,show.split('\\')[-1]) == None
               and re.search(rending,show.split('\\')[-1])
-              and re.search(ryear,show.split('\\')[-1])]
+              and (re.search(ryear,show.split('\\')[-1])
+                   or not re.search('(\d{1})',show.split('\\')[-1]))]
     return zip(movies)
 
    
