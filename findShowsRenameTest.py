ef40daae9202599ddd519026cd7a234f89997b54
perlahj <perlahj@gmail.com>
2016-05-08 16:48:17 +0000

    #01

--- findShowsRenameTest.py
+++ findShowsRenameTest.py
@@ -5,22 +5,25 @@ import re
 #rseason1 = '(S|s|Season|season)(\d{1,2})(([ -_\.]){0,1})(E|e|Episode|episode)(\d{1,2})|(\[)(\d{1,2})([.xX-_])(\d{1,2})(\])'
 #rseason2 = '(\[)(\d{1,2})([.xX-_])(\d{1,2})(\])|(S|s|Season|season)(\d{1,2})(([ -_\.]){0,1})(E|e|Episode|episode)(\d{1,2})'
 rseason1 = '(S|s|Season|season)(\d{1,2})([-_\.]*)(E|e|Episode|episode)(\d{1,2})'
-rseason2 = '(\[)(\d{1,2})([.xX-_])(\d{1,2})(\])'
+rseason2 = '(\[)*(\d{1,2})([.xX-_])(\d{1,2})(\])*'
+rseason3 = '(([-_\[#]+)(\d{1,3})(\]*))'
 
 def compressname(s):
-    return re.sub('[(){}<>\-\.; ]', '', s).lower()
+    return re.sub('[\[\](){}<>\+\-\.\_;_ 0-9]', '', s).lower()
 
 def compressseason(s):
     if re.search(rseason1,s):
+
         return "s" + re.search(rseason1,s).group(2) + "e" + re.search(rseason1,s).group(5)
-    elif re.search(rseason2,s).group(4):
+    elif re.search(rseason2,s):
+        #print(s)
         return "s" + re.search(rseason2,s).group(2) + "e" + re.search(rseason2,s).group(4)
     else:
-        return "nocompression"
+        return  "sx" + s[1:]
 
 def findshows(direct,s):
 
-    rseason = '(((S|s|Season|season)(\d{1,2})(E|e|Episode|episode)(\d{1,2}))|((\[)(\d{1,2})([.xX-_])(\d{1,2})(\])))'
+    rseason = '(((S|s|Season|season)(\d{1,2})([-_\.]*)(E|e|Episode|episode)(\d{1,2}))|((\[)(\d{1,2})([.xX-_])(\d{1,2})(\]))|(([-_#\[]+)(\d{1,3})(\])*))'
     rending = '((\.avi|\.mkv|\.mp4|\.rar){1})'
     regex = '(.+)(([ -_\.]){0,1})' + rseason + '(.*)' + rending
     #regexlong = '(.+)(([ -_\.]){0,1})(((S|s|Season|season)(\d{1,2})(E|e|Episode|episode)(\d{1,2}))|((\[)(\d{1,2})([.xX-_])(\d{1,2})(\])))(.*)((\.avi|\.mkv|\.mp4|\.rar){1})'
@@ -31,7 +34,7 @@ def findshows(direct,s):
     allshows = [os.path.join(root,file) for root,_,files in os.walk(direct) for file in files]
     sshows = [show for show in allshows if re.search(regex,show.split('\\')[-1]) != None
                      and name in compressname(re.search(regex,show).group(1).split('\\')[-1])]
-
+    
     newnames = [compressname(re.search(regex,show).group(1).split('\\')[-1]) for show in sshows]
     newseasons = [compressseason(re.search(rseason,show).group(1).split('\\')[-1]) for show in sshows]
     endings = [re.search(rending,show).group(1).split('\\')[-1] for show in sshows]
@@ -39,6 +42,7 @@ def findshows(direct,s):
 
 
     # Returns a list containg the names of the shows that was searched for and compressed filenames
-    return newseasons
+    return newshows
+
 
 
--- regexPathTestFile.py
+++ regexPathTestFile.py
@@ -22,7 +22,6 @@ def compressseason(s):
         return "s" + re.search(rseason2,s).group(2) + "e" + re.search(rseason2,s).group(4)
 
 # TO DO: 
-# TO DO: Leyfa . bil _- a milli e og s
 # TO DO: numer a thattum
 
     
@@ -33,7 +32,7 @@ def ff(direct,s):
     #rending = '((\.avi$|\.mkv$|\.rar$|\.mp4$){1})'
     #rseason = '((S|s|Season|season)(\d{1,2})(E|e|Episode|episode)(\d{1,2})|(\[\d{1,2}[.x]\d{1,2}\]))'
     #regex = r'(.*)([ -_\.]){1}' + rseason + '(.*)' + rending
-    rseason = '(((S|s|Season|season)(\d{1,2})(E|e|Episode|episode)(\d{1,2}))|((\[)(\d{1,2})([.xX-_])(\d{1,2})(\])))'
+    rseason = '(((S|s|Season|season)(\d{1,2})(E|e|Episode|episode)(\d{1,2}))|((\[)(\d{1,2})([.xX-_])(\d{1,2})(\]))|(([.#\[]+)(\d{1,3})(\]*)))'
     rending = '((\.avi|\.mkv|\.mp4|\.rar){1})'
     regex = '(.+)(([ -_\.]){0,1})' + rseason + '(.*)' + rending
     #regexlong = '(.+)(([ -_\.]){0,1})(((S|s|Season|season)(\d{1,2})(E|e|Episode|episode)(\d{1,2}))|((\[)(\d{1,2})([.xX-_])(\d{1,2})(\])))(.*)((\.avi|\.mkv|\.mp4|\.rar){1})'
@@ -48,11 +47,6 @@ def ff(direct,s):
     newseasons = [compressseason(re.search(rseason,show).group(1).split('\\')[-1]) for show in sshows]
     endings = [re.search(rending,show).group(1).split('\\')[-1] for show in sshows]
     newshows = [name + "." + season + ending for name, season, ending in zip(newnames,newseasons,endings)]
-
-    #newseasons = [compressseason(re.search(regex,show).group(2).split('\\')[-1]) for show in sshows]
-    #newendings = [re.search(regex,show).group(4).split('\\')[-1] for show in sshows]
-    #newshows = [a + '.' +b for a,b in zip(newnames,newseasons)]
-    #parsedshows =[re.findall(regex, show, re.VERBOSE) for show in sshows]
     
 
     # Returns the name of the shows that was searched for
